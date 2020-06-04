3b5c95435bbcf33b996978666e7d4830563e1c55
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 22:46:51 -0300

    Get all items from api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 
+import api from '../../services/api';
+
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint: React.FC = () => {
+
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get('items').then(res => {
+      setItems(res.data);
+    });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -98,30 +115,12 @@ const CreatePoint: React.FC = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
-              <span>Kitchen oil</span>
-            </li>
+            {items.map(item => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title}/>
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
