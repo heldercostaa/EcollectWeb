feb038cbb123d2b5ae035b637f1f1aa9d2b33431
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 22:27:12 -0300

    Add form and style of create-point page

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,8 +1,125 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import './styles.css';
 
 const CreatePoint: React.FC = () => {
   return (
-    <h1>Register</h1>
+    <div id="page-create-point">
+      <header>
+        <div id="logo">
+          <img src={logo} alt="E-collect" />
+          <h1>E-collect</h1>
+        </div>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Back to Home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Register new <br /> collection point</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Information</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Organization Name</label>
+            <input 
+              type="text"
+              name="name"
+              id="id"
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input 
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            
+            <div className="field">
+              <label htmlFor="name">Whatsapp</label>
+              <input 
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+              />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Address</h2>
+            <span>Pin an address on the map</span>
+          </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">State (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Choose a state</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <label htmlFor="city">City</label>
+              <select name="city" id="city">
+                <option value="0">Choose a city</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Collection Items</h2>
+            <span>Select one or more items</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="oil"/>
+              <span>Kitchen oil</span>
+            </li>
+          </ul>
+        </fieldset>
+
+        <button type="submit">
+          Register new point
+        </button>
+      </form>
+    </div>
   );
 }
 
