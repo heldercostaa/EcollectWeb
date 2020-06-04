e79ecad1fa220de2733c3faa11da4f42ac99f683
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 22:37:34 -0300

    Add map and pin with leaflet

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
@@ -63,6 +64,15 @@ const CreatePoint: React.FC = () => {
             <h2>Address</h2>
             <span>Pin an address on the map</span>
           </legend>
+
+          <Map center={[-3.7928829, -38.4955035]} zoom={15}>
+            <TileLayer 
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-3.7928829, -38.4955035]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
