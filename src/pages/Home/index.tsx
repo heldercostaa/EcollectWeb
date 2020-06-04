6f42d1dbdd575e037640badd3c4cc8c6b4af9705
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 21:59:39 -0300

    Change 'a' tag to Link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 import './styles.css'
@@ -17,12 +18,12 @@ const Home: React.FC = () => {
           <h1>Your marketplace to collect waste.</h1>
           <p>Our goal is to help people find collect points in an efficient way.</p>
         
-          <a href="/create-point">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Register a collect point</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
