47e2a41685a68d620b32ea91163e3cb3967121cf
Helder Costa <helderlpcosta@gmail.com>
2020-06-03 21:39:04 -0300

    Style Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import './styles.css'
 
 const Home: React.FC = () => {
-  return <h1>E-collect</h1>;
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="E-collect" />
+        </header>
+
+        <main>
+          <h1>Your marketplace to collect waste.</h1>
+          <p>Our goal is to help people find collect points in an efficient way.</p>
+        
+          <a href="/register">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Register a new collect point</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
